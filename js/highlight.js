c3d4d99d7b2ce972c583710f9a62daa0cc502522
Andrew <akv9509@saintjoe.edu>
2015-04-15 12:30:18 -0400

    Now has extra <pre>

---
+++ js/highlight.js
@@ -0,0 +1,33 @@
+var languages = {
+	  javascript: /\b(function|return|var|break|if)\b/g /* ... etc */
+	 // javascript: /\b\w*/
+};
+
+
+function highlightAllCode() {
+	//Gets what is in 'pre' and stores it in 'pres'
+  var pres = document.body.getElementsByTagName("pre");
+   // loops for the length of pres
+  for (var i = 0; i < pres.length; i++) {
+         // new pre variable is set for the current character in pres
+       var pre = pres[i];
+       //sets new variable lang to the attribute 'data-language' in pre
+
+       var lang = pre.getAttribute("data-language");
+
+       var name = pre.getAttribute("class-proof");
+
+    //   var foo = pre.getAttribute("fooness");
+
+       //If lang is a language
+
+       if (languages.hasOwnProperty(lang))
+
+	       //Highlight that code
+
+	       highlightCode(pre, languages[lang]);
+
+  }
+
+}
+
